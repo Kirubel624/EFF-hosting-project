b87a1570d6b4e2ec77d22457f8eb81032cf6f50c
Kirubel berhanu <kirubelberhanu@gmail.com>
2022-09-08 22:04:08 +0300

    backend

--- frontend/src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-console.log(process.env)
-export const baseURL = 'http://localhost:8000'
-const instance = axios.create({
-  baseURL: baseURL
-})
-
-export default instance
--- frontend/src/utils/formdata.js
+++
@@ -1,9 +0,0 @@
-export function createFormData(formData, key, data) {
-  if (data === Object(data) || Array.isArray(data)) {
-    for (var i in data) {
-      createFormData(formData, key + "[" + i + "]", data[i]);
-    }
-  } else {
-    formData.append(key, data);
-  }
-}
